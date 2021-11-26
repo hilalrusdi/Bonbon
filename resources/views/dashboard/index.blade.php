171f93b47f90d28387d8577fe220cfbaf8baad14
Nabila Syafa'ah <71918131+nabilasyafaah@users.noreply.github.com>
2021-11-26 10:23:43 +0700

    Update index.blade.php

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,10 +1,47 @@
 @extends('layout.main')
 
 @section('container')
+      <div class="head">
+            <h1>Dashboard</h1>
+      </div>
+<section class="cards-wrapper">
+  <div class="card-grid-space">
+    <a class="card" href="" style="--bg-img: url()">
+      <div>
+        <h1>Data Karyawan</h1>
+      </div>
+    </a>
+  </div>
+  <div class="card-grid-space">
+    <a class="card" href="" style="--bg-img: url()">
+      <div>
+        <h1>Data Supplier</h1>
+      </div>
+    </a>
+  </div>
+  <div class="card-grid-space">
+    <a class="card" href="" style="--bg-img: url()">
+      <div>
+        <h1>Data Tanaman</h1>
+      </div>
+    </a>
     
-    <h1>Dashboard</h1>
-    @can('admin')
-    <h2>Login as Admin</h2>
-    @endcan
-
+  </div>
+</section>
+<section class="cards-wrapper">
+  <div class="card-grid-space">
+    <a class="card" href="" style="--bg-img: url()">
+      <div>
+        <h1>Data Transaksi</h1>
+      </div>
+    </a>
+  </div>
+  <div class="card-grid-space">
+    <a class="card" href="" style="--bg-img: url()">
+      <div>
+        <h1>Quality Check</h1>
+      </div>
+    </a>
+  </div>
+</section>
 @endsection
